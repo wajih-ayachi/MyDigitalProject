540398a45781a0b55a2ef40bf8f8aa074747da41
Jrb Youssef <jrb.youssef@gmail.com>
2021-05-06 08:04:19 +0100

    Fix Drawer & Register

---
+++ src/views/Auth/Register.vue
@@ -0,0 +1,30 @@
+<template>
+<v-container fluid>
+    <v-layout row>
+      <v-col>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-col>
+      <v-col>
+        <h1 outline block class="#466F6E"> INSCRIPTION </h1>
+      </v-col>
+      <v-col class="bg" md6>
+        <v-img
+          lazy-src="../../assets/images/bg8.png"
+          max-height="500"
+          max-width="500"
+          src="../../assets/images/bg8.png">
+        </v-img>
+      </v-col>
+    </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
