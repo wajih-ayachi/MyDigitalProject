47eb6f362f2ab36c7d63da2d2d8433afac68fd15
Jrb Youssef <jrb.youssef@gmail.com>
2021-05-11 19:55:44 +0100

    Suit commit Vuex

---
+++ src/views/Home/ProfileSlider.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-carousel
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(user, i) in users"
+      :key="i"
+    >
+      <MiniProfile :user="user"/>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+import MiniProfile from '../../components/profile/MiniProfile'
+
+export default {
+  name: 'ProfileSlider',
+  props: ['users'],
+  components: { MiniProfile },
+  
+}
+</script>
